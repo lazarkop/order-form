d5a42e5b68370f0536709e88a9a6d1fc0ddf8153
Donatelo <lazar.kop@hotmail.com>
2023-05-02 14:56:11 +0200

    .

--- script.js
+++ script.js
@@ -80,3 +80,24 @@ function consoleLogFormData() {
   return false;
 }
 
+//ako input nije prazan zadrzi 'shrink' klasu na label elementima i 
+//dodaj 'add-color' klasu na input
+
+function addClassesIfInputIsNotEmpty() {
+  const inputs = document.querySelectorAll("input:not([type='submit'])");
+
+  function callback (e) {
+    const label = document.querySelector(`label[for="${e.target.id}"]`);
+    if(e.target.value !== ""){
+      label.className = 'form-input-label shrink';
+      e.target.className = 'form-input add-color';
+    } else {
+      label.className = 'form-input-label';
+      e.target.className = 'form-input';
+    }
+  }
+  inputs.forEach(input => {
+    input.addEventListener("input", callback)
+  });
+} 
+addClassesIfInputIsNotEmpty();
